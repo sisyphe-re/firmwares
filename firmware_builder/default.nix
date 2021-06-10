0404dd4b5767d82e8a9e4b327d3d993292e4063d
Rémy Grünblatt <remy@grunblatt.org>
2021-06-10 16:52:12 +0200

    Moving the RIOT package into the firmware builder

--- firmware_builder/default.nix
+++ firmware_builder/default.nix
@@ -1,12 +1,22 @@
-{ stdenv, lib, git, bash, python3, which, binutils, curl, gcc-arm-embedded, unzip, cacert, firmware_name, firmware_path, RIOT }:
+{ stdenv, fetchSWH, lib, git, bash, python3, which, binutils, curl, gcc-arm-embedded, unzip, cacert, firmware_name, firmware_path, outputHash }:
 
 stdenv.mkDerivation rec {
+  inherit outputHash;
+
   name = firmware_name;
-  src = firmware_path + ("/" + firmware_name);
+  srcs = [
+    (firmware_path + ("/" + firmware_name))
+    (fetchSWH {
+      swhid = "2d14aee3b8b21563a0900f4ac7e0c8f935a9449b";
+      sha256 = "1dzhnn6jqwpd5np24zss6wd7s4malq0fixwxjshm8p973bh5i865";
+      name = "RIOT";
+    })
+  ];
+
+  sourceRoot = "RIOT";
+
   APPLICATION = name;
-  RIOTBASE = RIOT;
   buildInputs = [
-    RIOT
     git
     bash
     python3
@@ -18,13 +28,26 @@ stdenv.mkDerivation rec {
     cacert
   ];
 
+  postPatch = ''
+    for x in $(find . -executable -type f); do
+      patchShebangs $x;
+    done
+    sed -i "s/\/usr\/bin\/env //g" makefiles/color.inc.mk
+  '';
+
   buildPhase = ''
-    OBJCOPY="arm-none-eabi-objcopy";
-    make
+    export OBJCOPY="arm-none-eabi-objcopy";
+    export RIOTBASE=../../
+    cp -r ../${name} examples/;
+    make -C examples/${name}/;
   '';
 
   installPhase = ''
     mkdir -p $out
-    cp bin/*/${APPLICATION}.elf $out/
+    cp examples/${name}/bin/*/${APPLICATION}.elf $out/
+    ls -alhR examples/${name}/
   '';
+
+  outputHashAlgo = "sha256";
+  outputHashMode = "recursive";
 }
--- flake.nix
+++ flake.nix
@@ -1,17 +1,16 @@
-
 {
   description = "A flake to build firmwares for RIOT OS";
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
-  inputs.RIOT.url = github:sisyphe-re/RIOT;
+  inputs.fetchSWH.url = github:sisyphe-re/fetchSWH;
 
-  outputs = { self, nixpkgs, RIOT }: {
+  outputs = { self, nixpkgs, fetchSWH }: {
     packages.x86_64-linux.gnrc_border_router =
       with import nixpkgs { system = "x86_64-linux"; };
-      callPackage ./firmware_builder { RIOT = RIOT.packages.x86_64-linux.RIOT; firmware_name = "gnrc_border_router"; firmware_path = ./src; };
+      callPackage ./firmware_builder { fetchSWH = fetchSWH.lib.fetchSWH; firmware_name = "gnrc_border_router"; firmware_path = ./src; outputHash = "sha256-1kQMis5kXJEelVyBI0ZHbtDGfWVvcHGv4bxsDHto1Z4="; };
 
     packages.x86_64-linux.gnrc_networking =
       with import nixpkgs { system = "x86_64-linux"; };
-      callPackage ./firmware_builder { RIOT = RIOT.packages.x86_64-linux.RIOT; firmware_name = "gnrc_networking"; firmware_path = ./src; };
+      callPackage ./firmware_builder { fetchSWH = fetchSWH.lib.fetchSWH; firmware_name = "gnrc_networking"; firmware_path = ./src; outputHash = "sha256-RSmEAqQ5Ohr9xkbHwAr/HpFAYlSzyAcIbQYvwmw7R0E="; };
   };
 }
