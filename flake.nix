331a9b530a39b00b4097b3c2579d647ce3cd8e45
Rémy Grünblatt <remy@grunblatt.org>
2021-09-25 18:35:11 +0200

    Updating the fetchSWH dependency

--- flake.nix
+++ flake.nix
@@ -2,16 +2,16 @@
   description = "A flake to build firmwares for RIOT OS";
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
-  inputs.fetchSWH.url = github:sisyphe-re/fetchSWH;
+  inputs.rgrunbla.url = github:rgrunbla/flakes;
 
-  outputs = { self, nixpkgs, fetchSWH }: {
+  outputs = { self, nixpkgs, rgrunbla }: {
     packages.x86_64-linux.gnrc_border_router =
       with import nixpkgs { system = "x86_64-linux"; };
-      callPackage ./firmware_builder { fetchSWH = fetchSWH.lib.fetchSWH; firmware_name = "gnrc_border_router"; firmware_path = ./src; outputHash = "sha256-fejWaUj+YzooTsIQ4qB+ZhJ/lZgMhaZa1FppNqoAyNQ="; };
+      callPackage ./firmware_builder { fetchSWH = rgrunbla.lib.fetchSWH; firmware_name = "gnrc_border_router"; firmware_path = ./src; outputHash = "sha256-fejWaUj+YzooTsIQ4qB+ZhJ/lZgMhaZa1FppNqoAyNQ="; };
 
     packages.x86_64-linux.gnrc_networking =
       with import nixpkgs { system = "x86_64-linux"; };
-      callPackage ./firmware_builder { fetchSWH = fetchSWH.lib.fetchSWH; firmware_name = "gnrc_networking"; firmware_path = ./src; outputHash = "sha256-Se6Dji2eLrdgGayRqJzzdKIsTKsom/bGLZVB5virhRQ="; };
+      callPackage ./firmware_builder { fetchSWH = rgrunbla.lib.fetchSWH; firmware_name = "gnrc_networking"; firmware_path = ./src; outputHash = "sha256-Se6Dji2eLrdgGayRqJzzdKIsTKsom/bGLZVB5virhRQ="; };
 
     packages.x86_64-linux.all-the-firmwares =
       with import nixpkgs { system = "x86_64-linux"; };
