9b762e2cac8df12958bb5a19ece7e82b43ad2ccd
Rémy Grünblatt <remy@grunblatt.org>
2021-11-21 10:14:53 +0100

    Formatting and removing FOD

--- firmware_builder/default.nix
+++ firmware_builder/default.nix
@@ -1,7 +1,21 @@
-{ stdenv, fetchSWH, lib, git, bash, python3, which, binutils, curl, gcc-arm-embedded, unzip, cacert, firmware_name, firmware_path, outputHash }:
+{ stdenv
+, fetchSWH
+, lib
+, git
+, bash
+, python3
+, which
+, binutils
+, curl
+, gcc-arm-embedded
+, unzip
+, cacert
+, firmware_name
+, firmware_path
+, fetchgit
+}:
 
 stdenv.mkDerivation rec {
-  inherit outputHash;
 
   name = firmware_name;
   srcs = [
@@ -11,22 +25,20 @@ stdenv.mkDerivation rec {
       sha256 = "1dzhnn6jqwpd5np24zss6wd7s4malq0fixwxjshm8p973bh5i865";
       name = "RIOT";
     })
+    (fetchgit {
+      url = "https://github.com/STMicroelectronics/cmsis_device_f1.git";
+      rev = "2948138428461c0621fd53b269862c6e6bb043ce";
+      sha256 = "sha256-orGO9ol2yDoEjQyGsUzQQjix+sK2qJBeOOx/5aR0myU=";
+      name = "cmsis_device_f1";
+      leaveDotGit = true;
+    })
   ];
 
   sourceRoot = "RIOT";
 
   APPLICATION = name;
-  buildInputs = [
-    git
-    bash
-    python3
-    which
-    curl
-    binutils
-    gcc-arm-embedded
-    unzip
-    cacert
-  ];
+  buildInputs =
+    [ git bash python3 which curl binutils gcc-arm-embedded unzip cacert ];
 
   patches = [ ./issue_16359.patch ./deter.patch ];
 
@@ -35,6 +47,11 @@ stdenv.mkDerivation rec {
       patchShebangs $x;
     done
     sed -i "s/\/usr\/bin\/env //g" makefiles/color.inc.mk
+    mkdir -p cpu/stm32/include/vendor/cmsis/f1/;
+    cp -a ../cmsis_device_f1/. cpu/stm32/include/vendor/cmsis/f1/;
+    chmod +w -R cpu/stm32/include/vendor/cmsis/f1/
+    echo '2948138428461c0621fd53b269862c6e6bb043ce' > cpu/stm32/include/vendor/cmsis/f1/.pkg-state.git-downloaded
+    find ./cpu/stm32/include/vendor/ -type f -exec md5sum {} \; &> log_orig.txt
   '';
 
   buildPhase = ''
@@ -42,6 +59,7 @@ stdenv.mkDerivation rec {
     export RIOTBASE=../../
     cp -r ../${name} examples/;
     make -C examples/${name}/;
+    find ./cpu/stm32/include/vendor/ -type f -exec md5sum {} \; &> log.txt
   '';
 
   installPhase = ''
@@ -50,6 +68,4 @@ stdenv.mkDerivation rec {
     ls -alhR examples/${name}/
   '';
 
-  outputHashAlgo = "sha256";
-  outputHashMode = "recursive";
 }
